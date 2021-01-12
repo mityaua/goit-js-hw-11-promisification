444d0e9aea5bf6d77ad6920aadd5ba6ec972929c
Dmytro <mityaua@gmail.com>
2021-01-12 23:24:00 +0200

    Добавляет предварительное решение первых двух задач

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './sass/styles.scss';
+import './js/task-1';
+import './js/task-2';
+// import './js/task-3';
---
+++ src/js/task-2.js
@@ -0,0 +1,26 @@
+// Массив обьектов
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+// Функция, возвращающая промис
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+ 
+  return new Promise((resolve) => {
+    resolve(updatedUsers);
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+// Вызовы функции для проверки
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
+
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
