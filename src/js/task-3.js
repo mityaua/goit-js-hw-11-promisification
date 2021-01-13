b71fc662fc77c0bc61421c8ecd1e96eecc7a1454
Dmytro <mityaua@gmail.com>
2021-01-13 15:59:49 +0200

    Fix time value

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -10,15 +10,18 @@ const makeTransaction = transaction => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
+        // resolve([transaction.id, delay]);
       }
 
       reject(transaction.id);
+      // reject([transaction.id]);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
+// const logSuccess = ( [id, time] )
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
